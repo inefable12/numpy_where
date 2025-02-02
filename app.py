8697159cacfe8f9156079a8c980e71872def2ec3
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2025-02-01 23:26:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 st.title("Aplicación de NUMPY - WHERE")
 st.info("np.where(condicion, TRUE, FALSE)")
 
-tabla = st.input_text("Ingresa tu csv:", "https://raw.githubusercontent.com/inefable12/numpy_where/refs/heads/main/midataset.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/inefable12/numpy_where/refs/heads/main/midataset.csv")
 
-df = pd.read_csv(tabla)
-st.show(df)
+st.write(df)
